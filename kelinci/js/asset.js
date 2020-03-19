72d9b55010c00ede8786c151d8413798c62866f4
Ramadhan Febrian <rfdhan@gmail.com>
2020-03-19 12:19:11 +0700

    improv

---
+++ kelinci/js/asset.js
@@ -0,0 +1,70 @@
+// // scroll
+// $('a[href*="#"]')
+//     // Remove links that don't actually link to anything
+//     .not('[href="#"]')
+//     .not('[href="#0"]')
+//     .click(function (event) {
+//         // On-page links
+//         if (
+//             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+//             &&
+//             location.hostname == this.hostname
+//         ) {
+//             // Figure out element to scroll to
+//             var target = $(this.hash);
+//             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//             // Does a scroll target exist?
+//             if (target.length) {
+//                 // Only prevent default if animation is actually gonna happen
+//                 event.preventDefault();
+//                 $('html, body').animate({
+//                     scrollTop: target.offset().top
+//                 }, 500, 'easeInBack', function () {
+//                     // Callback after animation
+//                     // Must change focus!
+//                     var $target = $(target);
+//                     $target.focus();
+//                     if ($target.is(":focus")) { // Checking if the target was focused
+//                         return false;
+//                     } else {
+//                         $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+//                         $target.focus(); // Set focus again
+//                     };
+//                 });
+//             }
+//         }
+//     });
+
+
+// slider
+$('.slider-one').slick({
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next",
+    fade: true,
+});
+
+//Slider dua
+
+$('.slider-dua').slick({
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    prevArrow:".site-slider-dua .slide-btn .prev",
+    nextArrow:".site-slider-dua .slide-btn .next"
+  });
+
+  $(document).ready(function(){
+      var form = $(".login-form");
+      var status = false;
+      $("#login").click(function(event){
+          event.preventDefault();
+          if(status == false){
+              form.show();
+              status = true;
+          }else{
+              form.fadeOut();
+              status = false;
+          }
+      })
+  })
---
+++ kelinci/js/modiv.js
@@ -0,0 +1,55 @@
+// scroll
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 500, 'easeInBack', function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+
+
+// slider
+$('.slider-one').slick({
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next"
+});
+
+//Slider dua
+
+$('.slider-dua').slick({
+    slidesToShow: 4,
+    slidesToScroll: 5,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow:".site-slider-dua .slide-btn .prev",
+    nextArrow:".site-slider-dua .slide-btn .next"
+  });
+
